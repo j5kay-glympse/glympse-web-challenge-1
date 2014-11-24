87b8f936022d106b4218b6245af959fc63090cdd
Eric Malone <ejmalone@gmail.com>
2014-11-24 03:06:01 +0000

    Adding basic error checking in the requirejs layer. Also realized we shouldn't create the map until page loads.

--- app/src/main.js
+++ app/src/main.js
@@ -6,15 +6,39 @@ define(function(require, exports, module) {
         angular = require('angular');
 
     exports.$ = $;
-    
-    require('mapApp');
 
-    // forgot that the app shouldn't bootstrap immediately or else the 
-    // dependency won't exist.
+    requirejs.onError = function(err) {
+        console.log('onError', err);
+    };
+    
+    // delay initialization so that the map is ready
     // via http://stackoverflow.com/questions/16286605/ \
     // initialize-angularjs-service-with-asynchronous-data
     
     $(function() {
-        angular.bootstrap(document, ['mapApp']);
+
+        require(
+            ['mapApp'], 
+
+            function(mapApp) {
+                angular.bootstrap(document, ['mapApp']);
+            },
+
+            // some basic error handling.
+            // @see http://requirejs.org/docs/api.html#errors
+            function(err) {
+                $('#gen-error').removeClass('hidden').addClass('show');
+
+                if(window.console && window.console.error) {
+                    console.error(
+                        "Failure in mapApp. Error name '%s'. Message: %s", 
+                        err.name, 
+                        err.message || err.description 
+                    );
+                }
+
+            }
+        );
+        
     });
 });
