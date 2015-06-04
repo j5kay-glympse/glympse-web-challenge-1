7a7f08f7bef7d5605f97370c4d38cd6a0368d1ae
Jason Keimig <jason@jkeimig.com>
2015-06-04 05:04:06 -0700

    grunt serve tweak

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -1,12 +1,7 @@
 
-var metrics = require('famous-metrics');
-
 module.exports = function (grunt) {
   'use strict';
   grunt.registerTask('serve', function (target) {
-    if (metrics.getTracking()) {
-      metrics.track('grunt serve', {});
-    }
 
     if (target === 'dist') {
       return grunt.task.run(['build', 'connect:dist:keepalive']);
