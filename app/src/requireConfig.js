baa7505fc4a87f8f9e609feea7b2b3df3bd5be12
Nils Whitmont <nils.whitmont@gmail.com>
2015-08-08 16:05:51 -0700

    move bootstrap-css to local lib & bower config

--- app/src/requireConfig.js
+++ app/src/requireConfig.js
@@ -8,18 +8,18 @@ require.config({
         requirejs: '../lib/requirejs/require',
         almond: '../lib/almond/almond',
         jquery: '../lib/jquery/dist/jquery',
-		googlemaps: '../lib/googlemaps-amd/src/googlemaps',
-		async: '../lib/requirejs-plugins/src/async'
-
+        googlemaps: '../lib/googlemaps-amd/src/googlemaps',
+        async: '../lib/requirejs-plugins/src/async'
     },
     packages: [
 
     ],
-	googlemaps: {
-		params: {
-			key: 'AIzaSyAXjjpKlzLPVOMkPrdntY69JGG64lEegqY',
-			libraries: 'places'
-		}
-	}
+    googlemaps: {
+        params: {
+            key: 'AIzaSyAXjjpKlzLPVOMkPrdntY69JGG64lEegqY',
+            libraries: 'places'
+        }
+    }
 });
+
 require(['main']);
