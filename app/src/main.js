95e5f5a1c0c137cc37a8bcf710cb620907720a8d
Max Winderbaum <maxwinderbaum@gmail.com>
2015-10-22 22:42:38 -0700

    Added bootstrap and less support, including fancy auto-prefixer and css cleaner

--- app/src/main.js
+++ app/src/main.js
@@ -1,10 +1,10 @@
 /* globals define */
 define(function(require, exports, module) {
-    'use strict';
-    // import dependencies
-    var $ = require('jquery');
-    var ng = require('angular');
-    console.log(ng.merge({}, {test: 'big'}));
+	'use strict';
+	// import dependencies
+	var $ = require('jquery');
+	var ng = require('angular');
+	console.log(ng.merge({}, {test: 'big'}));
 
-	  console.log('Map Challenge! - size=' + $(document).width() + 'x' + $(document).height());
+	console.log('Map Challenge! - size=' + $(document).width() + 'x' + $(document).height());
 });
