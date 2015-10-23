a926960daf1a7bb40f239e5fb3c5cbbff1bb4e94
Max Winderbaum <maxwinderbaum@gmail.com>
2015-10-22 22:05:14 -0700

    Included angular, bower.json was missing famous-polyfills

--- app/src/main.js
+++ app/src/main.js
@@ -3,6 +3,8 @@ define(function(require, exports, module) {
     'use strict';
     // import dependencies
     var $ = require('jquery');
+    var ng = require('angular');
+    console.log(ng.merge({}, {test: 'big'}));
 
 	console.log('Map Challenge! - size=' + $(document).width() + 'x' + $(document).height());
 });
--- app/src/requireConfig.js
+++ app/src/requireConfig.js
@@ -1,13 +1,19 @@
 /*globals require*/
 require.config({
     shim: {
-
+        angular: {
+            deps: [
+                'jquery'
+            ],
+            exports: 'angular'
+        }
     },
     paths: {
         famous: '../lib/famous/src',
         requirejs: '../lib/requirejs/require',
         almond: '../lib/almond/almond',
-        jquery: '../lib/jquery/dist/jquery'
+        jquery: '../lib/jquery/dist/jquery',
+        angular: '../lib/angular/angular'
     },
     packages: [
 
