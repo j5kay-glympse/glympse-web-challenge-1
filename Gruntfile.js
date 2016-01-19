962dc79da3f986c75dcc9defeb3bd03662e19c63
Kevin Nider <knider@hotmail.com>
2016-01-18 23:33:23 -0800

    submission

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,20 +3,20 @@
 /* Generated initially from grunt-init, heavily inspired by yo webapp */
 
 module.exports = function(grunt) {
-  'use strict';
+	'use strict';
 
-  // Time how long tasks take. Can help when optimizing build times
-  require('time-grunt')(grunt);
+	// Time how long tasks take. Can help when optimizing build times
+	require('time-grunt')(grunt);
 
-  // Load grunt config
-  require('load-grunt-config')(grunt, {
-    init: true,
-    data: {
-      config: {
-        // Configurable paths
-        app: 'app',
-        dist: 'dist'
-      }
-    }
-  });
+	// Load grunt config
+	require('load-grunt-config')(grunt, {
+		init: true,
+		data: {
+			config: {
+				// Configurable paths
+				app: 'app',
+				dist: 'dist'
+			}
+		}
+	});
 };
